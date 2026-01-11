3e00cc76e00abf5bb6dcf6c6df770b41ccc7735b
leezan <leezandawzdi@gmail.com>
2026-01-11 15:04:54 +0545

    refactoring

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,14 +1,16 @@
 import _4LinkedLists.*;
 
 public class MainProgram {
-    public static void main(String[] args) {
+    static void linkedListProgram() {
         try {
+
             int index = 2;
             System.out.println();
             LinkedList<Integer> linkedList = new Doubly<Integer>(0);
             linkedList.displayList();
 
-            System.out.println("\nThe removed element from end of list is: " + linkedList.removeEndNode());
+            System.out.println("\nThe removed element from end of list is: " +
+                    linkedList.removeEndNode());
             linkedList.addFrontNode(10);
             linkedList.displayList();
             linkedList.addEndNode(15);
@@ -17,16 +19,21 @@ public class MainProgram {
             linkedList.displayList();
 
             // linkedList.rotate();
-            System.out.println("\nThe element in front is: " + linkedList.getfrontNodeElement());
-            System.out.println("\nThe element in index " + index + " is: " + linkedList.getNthNodeElement(index));
+            System.out.println("\nThe element in front is: " +
+                    linkedList.getfrontNodeElement());
+            System.out.println("\nThe element in index " + index + " is: " +
+                    linkedList.getNthNodeElement(index));
 
-            System.out.println("\nThe removed element from front of list is: " + linkedList.removeFrontNode());
+            System.out.println("\nThe removed element from front of list is: " +
+                    linkedList.removeFrontNode());
             linkedList.displayList();
             System.out.println(
-                    "\nThe removed element from index " + index + " of list is: " + linkedList.removeNthNode(index));
+                    "\nThe removed element from index " + index + " of list is: " +
+                            linkedList.removeNthNode(index));
             linkedList.displayList();
             linkedList.displayList();
-            System.out.println("\nThe element in end is: " + linkedList.getEndNodeElement());
+            System.out.println("\nThe element in end is: " +
+                    linkedList.getEndNodeElement());
 
         } catch (Exception e) {
 
@@ -34,4 +41,7 @@ public class MainProgram {
         }
     }
 
+    public static void main(String[] args) {
+        linkedListProgram();
+    }
 }
